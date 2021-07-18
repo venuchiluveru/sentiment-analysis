2f6ac66d460ddcf839711a5473f260951f77f069
venuchiluveru <chiluveru.venu@gmail.com>
2021-07-19 00:14:41 +0530

    Sentiment Analysis

--- sentiment/sentiment/settings.py
+++ sentiment/sentiment/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'sentimentapp.apps.SentimentConfig',
+    'sentimentapp.apps.SentimentappConfig',
 ]
 
 ROOT_URLCONF = 'sentiment.urls'
