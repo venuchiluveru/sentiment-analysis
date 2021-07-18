b148c867c691bf2fef1204992e1bf02ab0108084
venuchiluveru <chiluveru.venu@gmail.com>
2021-07-19 00:28:24 +0530

    Sentiment Analysis

---
+++ sentiment/sentimentapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.tweet_list, name='tweet_list'),
+]
--- sentiment/sentimentapp/views.py
+++ sentiment/sentimentapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def tweet_list(request):
+    return render(request, 'sentimentapp/tweet_list.html', {})
